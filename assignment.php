1603003871d57601fc95afddb71ffb452681f3d9
prolay <prolaycsediu@gmail.com>
2018-11-27 12:35:30 +0600

    first commit

---
+++ assignment.php
@@ -0,0 +1,86 @@
+<?php
+
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <title></title>
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+		
+		<!--bootstrap css-->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="font-awesome.min.css">
+		
+		<link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+		
+		<!--custom css-->
+		<link rel="stylesheet" href="style.css">
+		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+
+	
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+	  <form class="form-inline" action="">
+		<input class="form-control mr-sm-2" type="text" placeholder="Search">
+		<input type="submit" name="submit" value="Search">
+	  </form>
+	</nav>
+	
+	<br>
+	<br>
+	
+	<p>
+		This paragraph contains a lot of lines in the source code,
+		but the browser ignores it.This paragraph contains a lot of lines in the source code,
+		but the browser ignores it.This paragraph contains a lot of lines in the source code,
+		but the browser ignores it.This paragraph contains a lot of lines in the source code,
+		but the browser ignores it.This paragraph contains a lot of lines in the source code,
+		but the browser ignores it.
+    </p>
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+		<!--java script link-->
+		<script src="js/bootstrap.min.js"></script>
+		
+        <script src="js/main.js"></script>
+		
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
